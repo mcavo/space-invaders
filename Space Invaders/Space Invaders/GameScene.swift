1c0a5cc68559b3f93f26e060014cbe8247eb21f5
Victoria <mvictoria.cavo@gmail.com>
2017-03-21 16:38:01 -0300

    Add projectiles

--- Space Invaders/Space Invaders/GameScene.swift
+++ Space Invaders/Space Invaders/GameScene.swift
@@ -8,33 +8,86 @@
 
 
 import SpriteKit
+import CoreMotion
 
-class GameScene: SKScene {
-    
+struct PhysicsCategory {
+    static let None      : UInt32 = 0
+    static let All       : UInt32 = UInt32.max
+    static let Invader   : UInt32 = 0b1       // 1
+    static let Projectile: UInt32 = 0b10      // 2
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     let spaceInvadersPerRow = 8
-    //let motionManager = CMMotionManager()
-    
+    // *****
+    let motionManager = CMMotionManager()
+    // *****
     let kShipName = "SpaceShip"
     let kSpaceInvaderName = "SpaceInvader"
-    let kscoreName = "Score"
+    let kScoreName = "Score"
+    
+    let kProjectileVelocity = CGVector(dx: 0, dy: 100)
     
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
+        physicsWorld.gravity = CGVector.zero
+        physicsWorld.contactDelegate = self
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         addSpaceInvaders()
         addSpaceShip()
-        //motionManager.startAccelerometerUpdates()
-        
+        // *****
+        motionManager.startAccelerometerUpdates()
+        // *****
         }
     
     override func update(_ currentTime: TimeInterval) {
+        processSpaceShipMotion(forUpdate: currentTime)
+        updateProjectiles()
+    }
+    
+    func updateProjectiles() {
+        self.enumerateChildNodes(withName: "Projectile") {
+            node, stop in
+            node.physicsBody!.velocity = self.kProjectileVelocity
+            if node.position.y > self.size.height - 2 * node.frame.size.height {
+                node.removeFromParent()
+            }
+        }
         
+        if let ship = childNode(withName: kShipName) as? SKSpriteNode {
+            if let data = motionManager.accelerometerData {
+                if fabs(data.acceleration.x) > 0.2 {
+                    ship.physicsBody!.applyForce(CGVector(dx: 10 * CGFloat(data.acceleration.x), dy: 0))
+                }
+            }
+        }
     }
     
     func addSpaceShip() {
         let ship = Ship(imageNamed: "SpaceShip")
         ship.position = CGPoint(x: size.width * 0.5, y: size.height * 0.1)
         ship.name = kShipName
+        // *****
+        ship.physicsBody = SKPhysicsBody(rectangleOf: ship.frame.size)
+        ship.physicsBody!.isDynamic = true
+        ship.physicsBody!.affectedByGravity = false
+        ship.physicsBody!.mass = 0.01
+        ship.physicsBody!.allowsRotation = false
+        // *****
         addChild(ship)
+    }
+    
+    func addProjectile() {
+        if let ship = childNode(withName: kShipName) {
+            let projectile = Projectile(imageNamed: "Projectile")
+            projectile.position = CGPoint(x: ship.position.x, y: ship.position.y + ship.frame.size.height * 0.5 + projectile.size.height)
+            projectile.name = "Projectile"
+            projectile.physicsBody?.categoryBitMask = PhysicsCategory.Projectile
+            projectile.physicsBody?.contactTestBitMask = PhysicsCategory.Invader
+            projectile.physicsBody?.collisionBitMask = PhysicsCategory.None
+            projectile.physicsBody?.usesPreciseCollisionDetection = true
+            addChild(projectile)
+        }
     }
     
     func addSpaceInvaders() {
@@ -57,6 +110,9 @@ class GameScene: SKScene {
             let positionY = CGFloat(Double(size.height * 0.6) + Double(row) * 1.5 * Double(invader.size.height))
             invader.position = CGPoint(x: positionX, y: positionY)
             invader.name = kSpaceInvaderName
+            invader.physicsBody?.categoryBitMask = PhysicsCategory.Invader
+            invader.physicsBody?.contactTestBitMask = PhysicsCategory.Projectile
+            invader.physicsBody?.collisionBitMask = PhysicsCategory.None
             addChild(invader)
             invader.run(SKAction.repeatForever(
                 SKAction.animate(with: frames,
@@ -77,6 +133,52 @@ class GameScene: SKScene {
         }
         
         return kFrames
+    }
+    // *****
+    func processSpaceShipMotion(forUpdate currentTime: CFTimeInterval) {
+        if let ship = childNode(withName: kShipName) as? Ship {
+            if let data = motionManager.accelerometerData {
+                if fabs(data.acceleration.x) > 0.2 {
+                    ship.physicsBody!.applyForce(CGVector(dx: 10 * CGFloat(data.acceleration.x), dy: 0))
+                }
+            }
+        }
+    }
+    // *****
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        addProjectile()
+    }
+    
+    func projectileDidCollideWithInvader(projectile: SKSpriteNode, invader: SKSpriteNode) {
+        // TODO: Add Score
+        print("Hit")
+        projectile.removeFromParent()
+        invader.removeFromParent()
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        // 1
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        // 2
+        if ((firstBody.categoryBitMask & PhysicsCategory.Invader != 0) &&
+            (secondBody.categoryBitMask & PhysicsCategory.Projectile != 0)) {
+            if let invader = firstBody.node as? SKSpriteNode, let
+                projectile = secondBody.node as? SKSpriteNode {
+                projectileDidCollideWithInvader(projectile: projectile, invader: invader)
+            }
+        }
+        
     }
     
 }
--- Space Invaders/Space Invaders/Projectile.swift
+++ Space Invaders/Space Invaders/Projectile.swift
@@ -1,30 +1,26 @@
 //
-//  SpaceInvader.swift
+//  Projectile.swift
 //  Space Invaders
 //
-//  Created by María Victoria Cavo on 17/3/17.
+//  Created by María Victoria Cavo on 21/3/17.
 //  Copyright © 2017 María Victoria Cavo. All rights reserved.
 //
 
 import SpriteKit
 
-class SpaceInvader : SKSpriteNode {
+class Projectile : SKSpriteNode {
     
-    var points : Int = 0
-    
-    
-    init(imageTexture: SKTexture) {
+    init(imageNamed: String) {
+        let imageTexture = SKTexture(imageNamed: imageNamed)
         super.init(texture: imageTexture, color: UIColor.clear, size: imageTexture.size())
         self.physicsBody = SKPhysicsBody(rectangleOf: imageTexture.size())
         self.physicsBody?.isDynamic = true
         self.physicsBody?.isResting = false
         self.physicsBody?.affectedByGravity = false
-        //self.physicsBody?.velocity = CGVector(dx: 8, dy: 0)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-
 }
